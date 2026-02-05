a9067b0b2d96bc9b264acd57a85d2ea0fcefa813
Anton <grygorenko23@gmail.com>
2026-02-05 11:04:05 +0100

    decode SECRET_KEY_ACCESS and SECRET_KEY_REFRESH to string to satisfy Pydantic validation

--- src/config/settings.py
+++ src/config/settings.py
@@ -44,7 +44,7 @@ class Settings(BaseAppSettings):
     POSTGRES_DB_PORT: int = int(os.getenv("POSTGRES_DB_PORT", 5432))
     POSTGRES_DB: str = os.getenv("POSTGRES_DB", "test_db")
 
-    SECRET_KEY_ACCESS: str = os.getenv("SECRET_KEY_ACCESS",  binascii.hexlify(os.urandom(32)).decode())
+    SECRET_KEY_ACCESS: str = os.getenv("SECRET_KEY_ACCESS", binascii.hexlify(os.urandom(32)).decode())
     SECRET_KEY_REFRESH: str = os.getenv("SECRET_KEY_REFRESH", binascii.hexlify(os.urandom(32)).decode())
     JWT_SIGNING_ALGORITHM: str = os.getenv("JWT_SIGNING_ALGORITHM", "HS256")
 
